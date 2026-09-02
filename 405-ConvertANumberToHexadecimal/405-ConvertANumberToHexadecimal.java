91857ebf2ea7eb7ae0d64ff55d70a1c12f11f50b
MahasmritiSS <ssmahasmriti@gmail.com>
2026-09-02 12:22:45 +0530

    Create 405-ConvertANumberToHexadecimal

---
+++ 405-ConvertANumberToHexadecimal/405-ConvertANumberToHexadecimal.java
@@ -0,0 +1,20 @@
+// Last updated: 9/2/2026, 12:22:53 PM
+class Solution {
+    public String toHex(int num) {
+
+        if (num == 0) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder(8);
+        char[] hex = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+        while (num != 0) {
+
+            sb.append(hex[num & 0b1111]);
+            num >>>= 4;
+        }
+
+        return sb.reverse().toString();
+    }
+}
